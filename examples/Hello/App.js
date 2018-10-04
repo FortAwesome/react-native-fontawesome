506fc569ee56b44e4ecb4f21aee6b56dcd70dd0b
Mike Wilkerson <mwilkerson@gmail.com>
2018-10-04 11:30:54 -0500

    Clean up example app

--- examples/Hello/App.js
+++ examples/Hello/App.js
@@ -1,33 +1,18 @@
 import React from 'react'
 import { Dimensions, StyleSheet, Text, ScrollView } from 'react-native'
-import { Svg, Path } from 'react-native-svg'
+import { Svg, Path, ClipPath, Defs, Mask, G, Rect } from 'react-native-svg'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { fas } from '@fortawesome/pro-solid-svg-icons'
-import { far } from '@fortawesome/pro-regular-svg-icons'
-import { fal } from '@fortawesome/pro-light-svg-icons'
+import { faCoffee, faBeer } from '@fortawesome/free-solid-svg-icons'
 
 const { width, height } = Dimensions.get('window')
 
 export default class App extends React.Component {
   render() {
     return (
-      <ScrollView style={styles.container}>
-        <Text>Show me the icons:</Text>
-        {
-         Object.keys(fas).map(icon => {
-           return <FontAwesomeIcon key={`fas-${fas[icon].iconName}`} icon={ fas[icon] }/>
-         })
-        }
-        {
-         Object.keys(far).map(icon => {
-           return <FontAwesomeIcon key={`far-${far[icon].iconName}`} icon={ far[icon] }/>
-         })
-        }
-        {
-         Object.keys(fal).map(icon => {
-           return <FontAwesomeIcon key={`fal-${fal[icon].iconName}`} icon={ fal[icon] }/>
-         })
-        }
+      <ScrollView style={styles.contentContainer}>
+        <Text>Show me some icons:</Text>
+        <FontAwesomeIcon icon={ faCoffee } />
+        <FontAwesomeIcon icon={ faBeer } />
       </ScrollView>
     )
   }
@@ -35,6 +20,7 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   contentContainer: {
-    paddingVertical: 20
+    paddingVertical: 50,
+    marginLeft: 20
   },
 })

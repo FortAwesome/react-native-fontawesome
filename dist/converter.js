6ed8566bb116774c79c1c3cd20adeb116c5748e3
Mike Wilkerson <mwilkerson@gmail.com>
2018-10-12 15:39:32 -0700

    refresh dist

--- dist/converter.js
+++ dist/converter.js
@@ -49,11 +49,10 @@ function convert(createElement, element) {
   extraProps.style,
       modifiedExtraProps = _objectWithoutProperties(extraProps, ["style"]); // If a color was passed in as a style sheet on the style prop, set the fill attribute to its value.
   // This is a prop we'll want to pass down to children as well.
-
-
-  if (extraProps.style && extraProps.style.color) {
-    modifiedExtraProps['fill'] = extraProps.style.color;
-  } // We don't want to pass down height/width props to children: they're only intended for the
+  // if(extraProps.style && extraProps.style.color){
+  //   modifiedExtraProps['fill'] = extraProps.style.color
+  // }
+  // We don't want to pass down height/width props to children: they're only intended for the
   // top-level element.
 
 

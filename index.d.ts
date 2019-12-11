8e240af956270ce577e81d32f67e860920c11ae1
Dizy <mhisf@vip.qq.com>
2019-12-12 02:12:00 +0800

    Add types (#17)

---
+++ index.d.ts
@@ -0,0 +1,25 @@
+import { StyleProp, ViewStyle } from "react-native";
+import { Transform, IconProp } from "@fortawesome/fontawesome-svg-core";
+
+export type FontAwesomeIconStyle = StyleProp<ViewStyle> & {
+  color?: string;
+};
+
+export interface Props {
+  icon: IconProp;
+  /**
+   * @deprecated
+   */
+  height?: number;
+  /**
+   * @deprecated
+   */
+  width?: number;
+  size?: number;
+  color?: string;
+  mask?: IconProp;
+  transform?: string | Transform;
+  style?: FontAwesomeIconStyle;
+}
+
+export function FontAwesomeIcon(props: Props): JSX.Element;

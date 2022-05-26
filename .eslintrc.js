0fd97f1d29cba4ed5265bef11cd52cbb9fd54021
robmadole <robmadole@gmail.com>
2022-05-26 12:31:39 -0500

    Adding eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    'jest/globals': true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'jest'
+  ],
+  rules: {
+  }
+}

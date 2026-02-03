c89d629b30c0f11e7cc64dd1614a961cf67b35b2
Rob Madole <robmadole@gmail.com>
2026-02-03 12:19:02 -0600

    Fix mask bug in iOS, better examples

---
+++ example/src/settings.ts
@@ -0,0 +1,15 @@
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+// oklch colors converted to hex approximations
+export const colors = [
+  '#5BBD5B', // oklch(76.4% 0.184 143)
+  '#0D5C0D', // oklch(36.4% 0.202 143)
+  '#5B5FC7', // oklch(56.4% 0.175 263)
+  '#3D4180', // oklch(41.4% 0.129 263)
+  '#C44536', // oklch(51.4% 0.175 22.5)
+  '#CF7966', // oklch(61.4% 0.129 22.5)
+];
+
+export const maskShapes = [fas.faCircle, fas.faSquare];
+
+export const ICON_COUNT = 48;
